38fedc3a8b9bebe08130bcbf5bbdcd0bbdf3ee93
jisan5620jk <jisank5620@gmail.com>
2024-09-11 13:10:21 +0600

    Update Banner component

--- src/Component1/Banner/Banner.jsx
+++ src/Component1/Banner/Banner.jsx
@@ -4,11 +4,19 @@ import bannerTeam from "/images/team-img.png";
 import bannerCheck from "/images/tick.png";
 import { Link } from "react-router-dom";
 import { GoArrowRight } from "react-icons/go";
+import { FaInstagram, FaLinkedinIn, FaPinterestP, FaSquareFacebook } from "react-icons/fa6";
 
 const Banner = () => {
   return (
     <div className="bg-BodyBg-0 px-2 lg:px-[30px]">
       <section className="bg-[url('/images/hero-bg.png')] bg-cover bg-center bg-no-repeat h-[600px] sm:h-[700px] md:h-[1100px] lg:h-[700px] xl:h-[790px] 2xl:h-[790px] flex items-center relative z-10 overflow-hidden rounded-t-[40px]">
+        <div className="absolute top-1/2 -translate-y-1/2 -left-10">
+          <Link to={"/"}>
+            <button className="font-AlbertSans font-medium inline-block text-lg text-white rotate-90 relative before:absolute before:-left-[72px] before:-bottom-[35px] before:rotate-90 before:w-[2px] before:h-[100px] before:bg-[linear-gradient(_180deg,_rgba(255,_255,_255,_0.9999999999999999)_0%,_rgba(27,_31,_73,_0)_100%_)]">
+              +980 123 (4567) 890
+            </button>
+          </Link>
+        </div>
         <div className="flex justify-center items-center m-auto">
           <div className="Container">
             <div className="relative z-10">
@@ -18,8 +26,12 @@ const Banner = () => {
                 </h1>
                 <img src={bannerHeart} draggable="false" />
               </div>
-              <div className="absolute z-10 left-1/2 -translate-x-1/2 top-0">
-                <img src={bannerThumb} draggable="false" />
+              <div className="absolute z-10 left-1/2 -translate-x-1/2 -top-28">
+                <img
+                  src={bannerThumb}
+                  draggable="false"
+                  className="max-w-[inherit]"
+                />
               </div>
               <div className="flex items-center justify-center gap-6 relative z-20 -mt-8">
                 <div className="bg-white bg-opacity-20 backdrop-filter filter-blur-[5px] backdrop-blur-lg flex items-center gap-[15px] rounded-full py-[14px] px-5">
@@ -49,14 +61,46 @@ const Banner = () => {
               </div>
               <div>
                 <Link to={"/appiontment"}>
-                  <button className="font-AlbertSans font-medium text-lg text-white flex items-center gap-2 bg-PrimaryColor-0 px-9 py-[18px] rounded-full">
+                  <button className="font-AlbertSans font-medium text-lg text-white flex items-center gap-[4px] bg-PrimaryColor-0 px-9 py-[18px] rounded-full relative z-10">
                     Free Consultation
-                    <GoArrowRight />
+                    <GoArrowRight size={"22"} className="-rotate-45" />
                   </button>
                 </Link>
               </div>
             </div>
           </div>
+        </div>
+        <div className="absolute top-1/2 -translate-y-1/2 right-10">
+          <ul className="flex flex-col items-center gap-[26px]">
+            <li>
+              <Link to={"/"}>
+                <button className="text-white">
+                  <FaSquareFacebook size={"20"} />
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <button className="text-white">
+                  <FaLinkedinIn size={"20"} />
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <button className="text-white">
+                  <FaPinterestP size={"20"} />
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <button className="text-white">
+                  <FaInstagram size={"20"} />
+                </button>
+              </Link>
+            </li>
+          </ul>
         </div>
       </section>
     </div>
