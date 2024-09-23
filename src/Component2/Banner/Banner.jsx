28607644c6f35ecbf0610b71b72da1dda396d6b0
jisan5620jk <jisank5620@gmail.com>
2024-09-23 11:00:53 +0600

    Update

--- src/Component2/Banner/Banner.jsx
+++ src/Component2/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import { GoArrowRight } from 'react-icons/go';
 const Banner = () => {
   return (
     <div className='bg-BodyBg-0 px-2 lg:px-[30px]'>
-      <section className='bg-[url(/images/hero-bg2.png)] bg-cover bg-no-repeat h-[600px] sm:h-[700px] md:h-[700px] lg:h-[700px] xl:h-[850px] 2xl:h-[850px] flex items-center relative z-10 overflow-hidden rounded-t-2xl md:rounded-[30px]'>
+      <section className='bg-[url(/images/hero-bg2.png)] bg-cover bg-no-repeat h-[650px] sm:h-[700px] md:h-[700px] lg:h-[700px] xl:h-[850px] 2xl:h-[850px] flex items-center relative z-10 overflow-hidden rounded-t-2xl md:rounded-[30px]'>
         <div className='absolute z-10 top-0 left-0 w-full md:w-[834px] md:h-[447px] inline-block bg-Secondarycolor-0 rounded-[30px] pl-8 md:pl-[100px] pt-[88px] pb-20'>
           <h1 className='font-AlbertSans font-semibold text-3xl leading-[40px] md:text-[70px] md:leading-[80px] text-white'>
             Bringing you the <br /> Best Healthcare
@@ -22,7 +22,7 @@ const Banner = () => {
             </Link>
           </div>
         </div>
-        <div className='absolute bottom-[100px] right-[100px] flex gap-5 bg-white bg-opacity-30 backdrop-filter backdrop-blur-md border-2 border-white rounded-[30px] pt-8 px-10 pb-9'>
+        <div className='absolute bottom-8 right-8 xl:bottom-[100px] xl:right-[100px] ml-8  flex flex-col sm:flex-row gap-5 bg-white bg-opacity-30 backdrop-filter backdrop-blur-md border-2 border-white rounded-[30px] pt-8 px-10 pb-9'>
           <div className='mt-[6px]'>
             <img
               src={bannerCheck}
