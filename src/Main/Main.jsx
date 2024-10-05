7f9b385f5cc757d107ab8f21df6f7a3028ff2891
jisan5620jk <jisank5620@gmail.com>
2024-10-05 21:26:49 +0600

    Update Code

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,8 +1,8 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../Shared/Navbar/Navbar";
-import ScrollToTop from "../Shared/ScrollToTop/ScrollToTop";
-import Footer from "../Shared/Footer/Footer";
-import Cursor from "../Shared/Cursor/Cursor";
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Shared/Navbar/Navbar';
+import ScrollToTop from '../Shared/ScrollToTop/ScrollToTop';
+import Footer from '../Shared/Footer/Footer';
+import Cursor from '../Shared/Cursor/Cursor';
 
 const Main = () => {
   return (
