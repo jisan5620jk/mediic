d46f94a3dcde8392b8428bd43221569722858510
jisan5620jk <jisank5620@gmail.com>
2024-10-06 12:58:28 +0600

    Update Code

--- src/Pages/InnerPage/AboutInner/AboutInner.jsx
+++ src/Pages/InnerPage/AboutInner/AboutInner.jsx
@@ -1,17 +1,17 @@
-import { FaArrowRightLong } from "react-icons/fa6";
-import BreadCrumb from "../../../Shared/BreadCrumb/BreadCrumb";
-import About from "./About";
-import Mission from "./Mission/Mission";
-import Testimonial from "./Testimonial/Testimonial";
-import Subscribe from "../../../Component1/Subscribe/Subscribe";
+import { FaArrowRightLong } from 'react-icons/fa6';
+import BreadCrumb from '../../../Shared/BreadCrumb/BreadCrumb';
+import About from './About';
+import Mission from './Mission/Mission';
+import Testimonial from './Testimonial/Testimonial';
+import Subscribe from '../../../Component1/Subscribe/Subscribe';
 
 const AboutInner = () => {
   return (
     <>
       <BreadCrumb
-        breadCrumbTitle={"About Us"}
+        breadCrumbTitle={'About Us'}
         breadCrumbIcon={<FaArrowRightLong />}
-        breadCrumbLink={"About Us"}
+        breadCrumbLink={'About Us'}
       />
       <About />
       <Mission />
