57e0409c9221fe83b548e1f6af0deaab6687bc5e
jisan5620jk <jisank5620@gmail.com>
2024-10-07 17:10:37 +0600

    Update Main Component

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,8 @@ import Preloader from './Shared/Preloader/Preloader.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <React.StrictMode>
-      {/* <HelmetProvider context={HelmetChanger}> */}
       <Preloader />
+      {/* <HelmetProvider context={HelmetChanger}> */}
       <RouterProvider router={router} />
       {/* </HelmetProvider> */}
     </React.StrictMode>
