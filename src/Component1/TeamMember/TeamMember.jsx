a9051b6ef9e50d0ca74bfc0b75a12b33f3007ce9
jisan5620jk <jisank5620@gmail.com>
2024-10-12 21:05:53 +0600

    Update code

--- src/Component1/TeamMember/TeamMember.jsx
+++ src/Component1/TeamMember/TeamMember.jsx
@@ -1,61 +1,61 @@
-import teamThumb from "/images/team.jpg";
-import teamThumb2 from "/images/team2.jpg";
-import teamThumb3 from "/images/team3.jpg";
-import TeamCard from "./TeamCard";
-import CountUp from "react-countup";
-import circleShape from "/images/crcle-bg.png";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { EffectCoverflow,Pagination } from "swiper/modules";
+import teamThumb from '/images/team.jpg';
+import teamThumb2 from '/images/team2.jpg';
+import teamThumb3 from '/images/team3.jpg';
+import TeamCard from './TeamCard';
+import CountUp from 'react-countup';
+import circleShape from '/images/crcle-bg.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 import {
   FaFacebookF,
   FaLinkedinIn,
   FaPinterestP,
   FaXTwitter,
-} from "react-icons/fa6";
+} from 'react-icons/fa6';
 
 const teamData = [
   {
     id: 1,
     teamThumb: teamThumb,
-    teamTitle: "Merina M. Banty",
+    teamTitle: 'Merina M. Banty',
     socialIcon: <FaFacebookF />,
     socialIcon2: <FaXTwitter />,
     socialIcon3: <FaLinkedinIn />,
     socialIcon4: <FaPinterestP />,
-    teamSubTitle: "Dentist",
+    teamSubTitle: 'Dentist',
   },
   {
     id: 2,
     teamThumb: teamThumb2,
-    teamTitle: "Jone D. Alexon",
+    teamTitle: 'Jone D. Alexon',
     socialIcon: <FaFacebookF />,
     socialIcon2: <FaXTwitter />,
     socialIcon3: <FaLinkedinIn />,
     socialIcon4: <FaPinterestP />,
-    teamSubTitle: "Therapist",
+    teamSubTitle: 'Therapist',
   },
   {
     id: 3,
     teamThumb: teamThumb3,
-    teamTitle: "Jisan Khan",
+    teamTitle: 'Jisan Khan',
     socialIcon: <FaFacebookF />,
     socialIcon2: <FaXTwitter />,
     socialIcon3: <FaLinkedinIn />,
     socialIcon4: <FaPinterestP />,
-    teamSubTitle: "Psychologist",
+    teamSubTitle: 'Psychologist',
   },
   {
     id: 4,
     teamThumb: teamThumb2,
-    teamTitle: "James E. Huey",
+    teamTitle: 'James E. Huey',
     socialIcon: <FaFacebookF />,
     socialIcon2: <FaXTwitter />,
     socialIcon3: <FaLinkedinIn />,
     socialIcon4: <FaPinterestP />,
-    teamSubTitle: "Dentist",
+    teamSubTitle: 'Dentist',
   },
 ];
 
@@ -67,7 +67,7 @@ const TeamMember = () => {
     initialSlide: 1,
     centeredSlides: true,
     autoplay: true,
-    effect: "coverflow",
+    effect: 'coverflow',
     coverflowEffect: {
       rotate: 50,
       stretch: 0,
@@ -98,32 +98,32 @@ const TeamMember = () => {
     },
   };
   return (
-    <section className="team-members pb-28 relative z-10 bg-BodyBg-0 overflow-hidden pt-[106px]">
-      <div className="Container">
-        <div className="grid grid-cols-1 gap-8 md:items-center md:grid-cols-2 border-b border-BorderColor3-0 pb-7">
+    <section className='team-members pb-28 relative z-10 bg-BodyBg-0 overflow-hidden pt-[106px]'>
+      <div className='Container'>
+        <div className='grid grid-cols-1 gap-8 md:items-center md:grid-cols-2 border-b border-BorderColor3-0 pb-7'>
           <div>
-            <h1 className="font-AlbertSans font-bold uppercase text-HeadingColor-0 text-xl leading-[30px] sm:text-3xl sm:leading-[40px] md:text-[40px] md:leading-[50px] lg:text-[50px] lg:leading-[60px] xl:text-[52px] xl:leading-[62px] 2xl:text-[60px] 2xl:leading-[70px]">
+            <h1 className='font-AlbertSans font-bold uppercase text-HeadingColor-0 text-xl leading-[30px] sm:text-3xl sm:leading-[40px] md:text-[40px] md:leading-[50px] lg:text-[50px] lg:leading-[60px] xl:text-[52px] xl:leading-[62px] 2xl:text-[60px] 2xl:leading-[70px]'>
               Meet Our Best <br />
               Doctors
             </h1>
           </div>
-          <div className="flex md:justify-end">
-            <div className="flex items-center gap-8 ">
+          <div className='flex md:justify-end'>
+            <div className='flex items-center gap-8 '>
               <div>
                 <CountUp
                   start={-11}
-                  end={"58"}
-                  suffix={"+"}
-                  className="font-AlbertSans text-3xl sm:text-4xl md:text-[60px] leading-[50px] text-HeadingColor-0 font-bold"
+                  end={'58'}
+                  suffix={'+'}
+                  className='font-AlbertSans text-3xl sm:text-4xl md:text-[60px] leading-[50px] text-HeadingColor-0 font-bold'
                 />
               </div>
-              <p className="font-AlbertSans font-medium text-lg text-HeadingColor-0 uppercase">
+              <p className='font-AlbertSans font-medium text-lg text-HeadingColor-0 uppercase'>
                 Top Expert <br /> Doctors
               </p>
             </div>
           </div>
         </div>
-        <div className="mt-10">
+        <div className='mt-10'>
           <Swiper
             {...settings}
             pagination={pagination}
@@ -142,7 +142,7 @@ const TeamMember = () => {
               }) => {
                 return (
                   <SwiperSlide key={id}>
-                    <div className="pb-[60px] pt-5">
+                    <div className='pb-[60px] pt-5'>
                       <TeamCard
                         teamThumb={teamThumb}
                         teamTitle={teamTitle}
@@ -160,11 +160,11 @@ const TeamMember = () => {
           </Swiper>
         </div>
       </div>
-      <div className="absolute -z-10 bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2">
+      <div className='absolute -z-10 bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2'>
         <img
           src={circleShape}
-          draggable="false"
-          className="max-w-[inherit] w-[inherit]"
+          draggable='false'
+          className='max-w-[inherit] w-[inherit]'
         />
       </div>
     </section>
